38fe1d21655bb8dae550637930c9497ccb839c61
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-09 12:31:06 +0100

    intial commit

---
+++ src/routes/index.routes.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+import {Router} from "express"
+const router = Router();
+
+router.get("/", (req:Request, res:Response, next:NextFunction) => {
+  res.json("All good in here");
+});
+
+export default router;
